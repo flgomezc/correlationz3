d407290d6feb38ee8c6a7c01cce7623f72f16c7b
flgomezc <fl.gomez10@uniandes.edu.co>
2015-03-30 21:38:46 -0400

    Reduce BolshoiP catalog and create RandomFieldCatalog

--- programs/reduce_catalog.py
+++ programs/reduce_catalog.py
@@ -12,3 +12,23 @@ Data = open('../data/SmallCatalog.csv', 'w')
 for line in SmallCatalog:
     Data.write(str(line[0])+','+str(line[1])+','+str(line[2])+'\n')
 Data.close()
+
+
+### Create Random Field Catalog
+
+RFCatalog = open('../data/RandomFieldCatalog.cvs', 'w')
+
+random.seed(0)
+BoxLength = 250 # Mpc h^-1
+
+M = 3      ### M times the number of data points
+Ndata=SmallCatalog[:,0].shape[0]
+Nrand=int(M*Ndata)
+
+RandomField = np.zeros([Nrand,3])
+for line in RandomField:
+    for k in range(3):
+        line[k] = random.random()*BoxLength
+        RFCatalog.write(str(line[k])+',')
+    RFCatalog.write('\n')
+RFCatalog.close()
