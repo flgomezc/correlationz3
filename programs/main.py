a11b7366201b5c3924273beb29cd34fce165add7
flgomezc <fl.gomez10@uniandes.edu.co>
2015-03-30 23:30:37 -0400

    Create reduced catalog (M>5x10^10Msun), create RandomFieldCatalog of the same size, calculate correlation function. This program is not parallelized.

--- programs/main.py
+++ programs/main.py
@@ -1,88 +1,127 @@
 import numpy as np
 import random
 import timeit
-
-start = timeit.default_timer()
-
 random.seed(0)
 
-M = 5      ### M times the number of data points
+T_start_global = timeit.default_timer()
 
-pos = np.loadtxt('../data/cells/0.cvs', delimiter=',', usecols=[0,1,2])
-N=pos[:,0].shape[0]
+RunningLog = open('running.log', 'w')
 
-Ntotal = 12.651e6
-Nbox = int(Ntotal/(25**3))
-Nrand=M*Nbox
-print 'N data   =', N
-print 'N random =', Nrand
+T_start_loading = timeit.default_timer()
+Data = np.loadtxt('../data/SmallCatalog.csv', delimiter=',', usecols=[0,1,2])
+T_stop_loading = timeit.default_timer()
+
+RunningLog.write('Correlation Function At z=3 Log\n\n')
+RunningLog.write('Loading Data: '+str(T_stop_loading - T_start_loading)+'s\n')
+
+BoxLength = 250 # Mpc h^-1
 
 
-dist = 10**(np.linspace(-2.125,2.125,18)) ## Bins
-DIST = np.logspace(-2,2,17)                   ## Center of Bins
-counterDD = np.zeros(17)
-counterDR = np.zeros(17)
-counterRR = np.zeros(17)
+Ndata=Data[:,0].shape[0]
+RunningLog.write('N data   ='+str(Ndata)+'\n')
 
+Bins = 16
+dist = 10**(np.linspace(-1.375,2.125,Bins)) ## Bins
+DIST = np.logspace(-2,2,13)                   ## Center of Bins
+counterDD = np.zeros(Bins-1)
+counterDR = np.zeros(Bins-1)
+counterRR = np.zeros(Bins-1)
+
+RunningLog.write("Distance Bins =\n"+str(dist)+'\n\n')
+
+
+
+T_start_RandomField = timeit.default_timer()
 #### Create Random Field
-RandomField = np.zeros([Nrand,3])
-for i in range(Nrand):
+RandomField = np.loadtxt('../data/RandomFieldCatalog.cvs', delimiter=',', usecols=[0,1,2])
+Nrand = RandomField[:,0].shape[0]
+RunningLog.write('N random ='+str(Nrand)+'\n\n')
+print 'RandomField', RandomField.shape
+T_stop_RandomField = timeit.default_timer()
+RunningLog.write('Loading RandomField: '+str(T_stop_RandomField - T_start_RandomField )+'s\n')
+RunningLog.write('RandomField.shape =' +str(RandomField.shape)+'\n')
+
+
+T_start_Countig_RR = timeit.default_timer()
+matrix = np.zeros([Nrand,7])
+for k in range(3):
+        matrix[:,k]   = RandomField[:,k]
+counterRR2 = np.zeros(Bins-1)
+for line in RandomField:
+    aux = 0
     for k in range(3):
-        RandomField[i,k] = random.random()*10 # Boxlength
+        matrix[:,k+3] = line[k]
+        aux += (matrix[:,k]-matrix[:,k+3])**2
 
-#print RandomField.shape
+    matrix[:,6] = np.sqrt(aux)
+    for m in range(counterRR2.size):
+        index = np.where((matrix[:,6]>=dist[m])&(matrix[:,6]<dist[m+1]))[0]
+        counterRR2[m] += index.shape
+counterRR2 = counterRR2/( 2*Nrand*(Nrand-1) )
+T_stop_Counting_RR = timeit.default_timer()
 
-#### Counting Pairs in Random Field
-for i in range(Nrand-1):
-    for j in range(i+1,Nrand):
-        r = 0.0
-        for k in range(3):
-            r+=(RandomField[i,k]-RandomField[j,k])**2
-        r = r**0.5
-        m = 0
-        for m in range(counterDD.size):
-            if (dist[m]<r) and (r<dist[m+1]):
-                counterRR[m] +=1
-counterRR = counterRR/( Nrand*(Nrand-1) ) #### For 1 box, already divided by 2
-
-print counterRR, '\n'
+RunningLog.write('Random-Random counting\nRR = \n'+str(counterRR2)+'\n\n')
+RunningLog.write('Random-Random counting time = '+
+                  str(T_stop_Counting_RR - T_start_Countig_RR)+'s\n')
+print 'RR counting time: ', T_stop_Counting_RR - T_start_Countig_RR,'s\n'
 
 
 
-### Counting Pairs in Data-Data
+T_start_Countig_DD = timeit.default_timer()
+matrix = np.zeros([Ndata,7])
+for k in range(3):
+        matrix[:,k]   = Data[:,k]
+counterDD = np.zeros(Bins-1)
+for line in Data:
+    aux = 0
+    for k in range(3):
+        matrix[:,k+3] = line[k]
+        aux += (matrix[:,k]-matrix[:,k+3])**2
 
-for i in range(N-1):
-    for j in range(i+1,N):
-        r = 0.0
-        for k in range(3):
-            r+=(pos[i,k]-pos[j,k])**2
-        r = r**0.5
-        m = 0
-        for m in range(counterDD.size):
-            if (dist[m]<r) and (r<dist[m+1]):
-                counterDD[m] +=1
-counterDD = counterDD*1.0/(N*(N-1)) #### For 1 box, already divided by 2
-print counterDD
+    matrix[:,6] = np.sqrt(aux)
+    for m in range(counterDD.size):
+        index = np.where((matrix[:,6]>=dist[m])&(matrix[:,6]<dist[m+1]))[0]
+        counterDD[m] += index.shape
+counterDD = counterDD/( 2*Nrand*(Nrand-1) )
+T_stop_Counting_DD = timeit.default_timer()
+
+RunningLog.write('Data-Data counting\nDD = \n'+str(counterDD)+'\n\n')
+RunningLog.write('Data-Data counting time = '+
+                  str(T_stop_Counting_DD - T_start_Countig_DD)+'s\n')
+print 'DD counting time: ', T_stop_Counting_DD - T_start_Countig_DD,'s\n'
 
 
-### Counting pairs Data Random
-for i in range(N-1):
-    for j in range(0,Nrand):
-        r = 0.0
-        for k in range(3):
-            r+=(pos[i,k]-RandomField[j,k])**2
-        r = r**0.5
-        m = 0
-        for m in range(counterDR.size):
-            if (dist[m]<r) and (r<dist[m+1]):
-                counterDR[m] +=1
-counterDR = counterDR*1.0/(N*Nrand)
-print counterDR
+T_start_Countig_DR = timeit.default_timer()
+matrix = np.zeros([Nrand,7])
+for k in range(3):
+        matrix[:,k]   = Data[:,k]
+counterDR = np.zeros(Bins-1)
+for line in RandomField:
+    aux = 0
+    for k in range(3):
+        matrix[:,k+3] = line[k]
+        aux += (matrix[:,k]-matrix[:,k+3])**2
 
-CorrFunc = (counterDD - 2*counterDR + counterRR)/ counterRR
+    matrix[:,6] = np.sqrt(aux)
+    for m in range(counterDR.size):
+        index = np.where((matrix[:,6]>=dist[m])&(matrix[:,6]<dist[m+1]))[0]
+        counterDR[m] += index.shape
+counterDR = counterDR/( 2*Ndata*Nrand)
+T_stop_Counting_DR = timeit.default_timer()
 
-print 'Correlation Function:\n'
-print CorrFunc
+RunningLog.write('Data-Random counting\nDR = \n'+str(counterDR)+'\n\n')
+RunningLog.write('Data-Random counting time = '+
+                  str(T_stop_Counting_DR - T_start_Countig_DR)+'s\n\n\n')
+print 'DR counting time: ', T_stop_Counting_DR - T_start_Countig_DR,'s\n'
 
-stop = timeit.default_timer()
-print stop-start, 'seconds\n'
+
+
+CorrFunc = (counterDD - 2*counterDR + counterRR)/counterRR
+RunningLog.write('Correlation Function\nDR = \n'+str(CorrFunc)+'\n\n')
+
+T_stop_global = timeit.default_timer()
+RunningLog.write('Global running time = '+
+                  str(T_stop_global - T_start_global)+'s\n\n\n')
+print 'Global running time: ', T_stop_global - T_start_global,'s\n'
+
+RunningLog.close()
--- programs/reduce_catalog.py
+++ programs/reduce_catalog.py
@@ -2,12 +2,10 @@ import numpy as np
 import random
 import timeit
 
-Catalog = np.loadtxt('../data/BolshoiP_bdmV.csv', skiprows=1, delimiter=',', usecols=[3,4,5,6])
-
+Catalog = np.loadtxt('../data/BolshoiP_bdmV.csv', skiprows=1, delimiter=',',
+                        usecols=[3,4,5,6])
 index = np.where(Catalog[:,3]>5*10**10)
-
 SmallCatalog = np.array(Catalog[index])
-
 Data = open('../data/SmallCatalog.csv', 'w')
 for line in SmallCatalog:
     Data.write(str(line[0])+','+str(line[1])+','+str(line[2])+'\n')
@@ -17,11 +15,9 @@ Data.close()
 ### Create Random Field Catalog
 
 RFCatalog = open('../data/RandomFieldCatalog.cvs', 'w')
-
 random.seed(0)
 BoxLength = 250 # Mpc h^-1
-
-M = 3      ### M times the number of data points
+M = 1      ### M times the number of data points
 Ndata=SmallCatalog[:,0].shape[0]
 Nrand=int(M*Ndata)
 
